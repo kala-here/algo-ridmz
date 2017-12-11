c445019d529720f3205e2c9fcb1e1fd322166add
Kala Wetzler <kala.wetzler@gmail.com>
2017-12-10 19:54:05 -0800

    Pass first test

--- award_budget_cuts.rb
+++ award_budget_cuts.rb
@@ -4,6 +4,13 @@ def find_grants_cap(grants_array, new_budget)
   arr = grants_array
   arr = arr.sort
   arr = arr.reverse!
-  p arr
 
+  # get the sum of the array to then see if we need to move forward
+  sum = 0
+  arr.each { |x| sum += x }
+
+  # if sum is less than or equal to new budget, we are done/can return first item in array
+  if sum <= new_budget
+    return arr.first
+  end
 end
