78f7cfccdaf06646b14cb2797f32d2806cc618e7
Kala Wetzler <kala.wetzler@gmail.com>
2017-12-10 20:23:50 -0800

    Get sum == new_budget every time... hmmm

--- award_budget_cuts.rb
+++ award_budget_cuts.rb
@@ -4,6 +4,7 @@ def find_grants_cap(grants_array, new_budget)
   arr = grants_array
   arr = arr.sort
   arr = arr.reverse!
+  n = arr.length
 
   # get the sum of the array to then see if we need to move forward
   sum = 0
@@ -11,6 +12,17 @@ def find_grants_cap(grants_array, new_budget)
 
   # if sum is less than or equal to new budget, we are done/can return first item in array
   if sum <= new_budget
-    return arr.first
+    return arr[0]
   end
+
+  # skip first item in array since we know it is too large to fit in new_budget
+  # see if making value of arr[0] = arr[1] makes the sum <= new_budget
+  for x in arr
+    sum = 0
+    until sum == new_budget
+      x = x.next
+      arr.each { |x| sum += 1 } # DRY this up
+    end
+  end
+  p sum
 end
