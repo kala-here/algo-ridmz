76c78f9481061455a631740ba367dc550f989d8e
Kala Wetzler <kala.wetzler@gmail.com>
2017-12-10 21:12:01 -0800

    Start to get in the right direction with assigning new values

--- award_budget_cuts.rb
+++ award_budget_cuts.rb
@@ -22,13 +22,28 @@ def find_grants_cap(grants_array, new_budget)
 
   # skip first item in array since we know it is too large to fit in new_budget
   # see if making value of arr[0] = arr[1] makes the sum <= new_budget
-  for x in arr
-    sum = 0
-    p x
-    until sum <= new_budget
-      x = arr[arr.index(x)+1]
-      calc_sum(arr, sum)
+  sum = 0
+  i = 0
+  while i < n-2
+    sum = calc_sum(arr, sum)
+    if sum <= new_budget
+      break
+    else
+      arr[i] = arr[i+1]
+      sum = calc_sum(arr,sum)
+      i += 1
     end
-    sum
   end
+  p '**********'
+  p arr
+  # sum = 0
+  # for x in arr
+  #   next_lowest_value = arr[arr.index(x)+1]
+  #   until sum <= new_budget
+  #     x = next_lowest_value
+  #     sum = calc_sum(arr, sum)
+  #   end
+  #   sum
+  # end
+  # arr[0]
 end
