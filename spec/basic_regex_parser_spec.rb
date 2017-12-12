78b82aa7d502477975ecef0dc881ead2f35dc021
Kala Wetzler <kala.wetzler@gmail.com>
2017-12-12 15:08:17 -0800

    Add 'do' to do-blocks

--- spec/basic_regex_parser_spec.rb
+++ spec/basic_regex_parser_spec.rb
@@ -3,7 +3,7 @@ require_relative '../basic_regex_parser'
 
 describe '#is_match' do
 
-  describe 'when both text and pattern are empty'
+  describe 'when both text and pattern are empty' do
     let(:text) { "" }
     let(:pattern) { "" }
     it 'returns true because they match' do
@@ -11,7 +11,7 @@ describe '#is_match' do
     end
   end
 
-  describe 'when both text and pattern do not match without symbols'
+  describe 'when both text and pattern do not match without symbols' do
     let(:text) { "aa" }
     let(:pattern) { "a" }
     it 'returns false because they do not match' do
@@ -19,7 +19,7 @@ describe '#is_match' do
     end
   end
 
-  describe 'when both text and pattern do match without symbols'
+  describe 'when both text and pattern do match without symbols' do
     let(:text) { "bb" }
     let(:pattern) { "bb" }
     it 'returns true because they do match' do
@@ -27,7 +27,7 @@ describe '#is_match' do
     end
   end
 
-  describe "when both text and pattern do match with '*' included"
+  describe "when both text and pattern do match with '*' included" do
     let(:text) { "" }
     let(:pattern) { "a*" }
     it 'returns true because they do match' do
@@ -35,7 +35,7 @@ describe '#is_match' do
     end
   end
 
-  describe "when both text and pattern don't match with '*' included"
+  describe "when both text and pattern don't match with '*' included" do
     let(:text) { "abbdbb" }
     let(:pattern) { "ab*d" }
     it 'returns false because text exceeds pattern' do
@@ -43,7 +43,7 @@ describe '#is_match' do
     end
   end
 
-  describe "when both text and pattern do match with '.' included"
+  describe "when both text and pattern do match with '.' included" do
     let(:text) { "aba" }
     let(:pattern) { "a.a" }
     it 'returns true because they do match with a dot' do
@@ -51,7 +51,7 @@ describe '#is_match' do
     end
   end
 
-  describe "when both text and pattern do match with '.' & '*' included"
+  describe "when both text and pattern do match with '.' & '*' included" do
     let(:text) { "acd" }
     let(:pattern) { "ab*c." }
     it 'returns true because they do match with a dot' do
@@ -59,7 +59,7 @@ describe '#is_match' do
     end
   end
 
-  describe "when both text and pattern do match with '.' included"
+  describe "when both text and pattern do match with '.' included" do
     let(:text) { "abaa" }
     let(:pattern) { "a.*a*" }
     it 'returns true because they do match with a dot' do
