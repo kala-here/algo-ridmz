11fbeeb3e13936476798be4ed15138f5ebed8371
Kala Wetzler <kala.wetzler@gmail.com>
2017-12-12 14:04:33 -0800

    Add logic for star

--- basic_regex_parser.rb
+++ basic_regex_parser.rb
@@ -17,3 +17,41 @@
 # base cases =
   # if (text == pattern) RETURN true
   # if (pattern does not include * or .) & (does not match text) RETURN false
+
+# else push into stacks: text_stack & patter_stack
+
+def recursive_compare(tex, patt)
+# if we have reached the end of both at the same time, they are a match
+  if tex.peak == nil && patt.peak == nil
+    return true
+  end
+# if we have reached the end of the text and pattern is not completed, they do not match
+  if text.peak == nil && patt.peak != nil
+    return false
+  end
+# If they are equal or a dot, pop both off and continue on to next
+  if tex.peak == patt.peak || patt.peak == '.'
+    tex.pop
+    patt.pop
+  end
+
+  if patt.peak == '*'
+    patt.pop
+    if tex.peak != patt.peak # if *'s left neighbor does not match text, pop off pattern and compare again
+      patt.pop
+      if tex.peak != patt.peak # now they must match in order to be a whole match
+        return false
+      end
+    end
+    if tex.peak == patt.peak # here we check to see if *'s left neighbor does match text, then pop off text items until they don't match, then pop off pattern & compare next two items
+      until tex.peak != patt.peak
+        tex.pop
+      end
+      if text.peak != patt.peak
+        patt.pop
+      end
+    end
+  end
+
+
+end
