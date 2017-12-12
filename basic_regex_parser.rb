166e50eb3317c4a4941873d27b661606efb39907
Kala Wetzler <kala.wetzler@gmail.com>
2017-12-12 13:34:56 -0800

    Record train of thought before switching approch completely

--- basic_regex_parser.rb
+++ basic_regex_parser.rb
@@ -5,3 +5,37 @@
 # In case you aren’t familiar with regular expressions, the function determines if the text and pattern are the equal, where the '.' is treated as a single a character wildcard (see third example), and '*' is matched for a zero or more sequence of the previous letter (see fourth and fifth examples). For more information on regular expression matching, see the Regular Expression Wikipedia page.
 
 # Explain your algorithm, and analyze its time and space complexities.
+# __________________________________________________________________
+
+# text = "abbbbbcd" OR "acg"
+# pattern = "ab*c."
+# output = true
+# temp pattern =
+
+# Pseudocode----------
+# I am thinking that after base cases there will be a case statement
+
+# base cases =
+  # if (text == pattern) RETURN true
+  # if (pattern does not include * or .) & (does not match text) RETURN false
+
+# if pattern includes both . & *
+  #
+
+# if pattern includes .
+  #
+
+# if pattern includes *
+  # make variables for item before star, item before that item, and item after star
+    # left_left_neighbor = pattern[star_index - 2]; left_neighbor = pattern[star_index - 1]; right_neighbor = pattern[star_index + 1]
+  # if any of these variable are a dot
+    #
+  # if item 2 before star is null, then text can start with item before star or start with item after star
+
+  # if text starts with item before star, there can be any number of those items in text before item.next has to be equal to item after start in order to RETURN true
+
+  # push text into array
+  # push pattern into array
+    # when item == *
+      # compare index to item at index of text array
+        # 
