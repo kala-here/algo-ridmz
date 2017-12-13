f9cfb9043d75c2a1b3e83075e7c5d0cdb0b30b2c
Kala Wetzler <kala.wetzler@gmail.com>
2017-12-13 07:39:27 -0800

    Try something new with a for loop

--- number_of_paths.rb
+++ number_of_paths.rb
@@ -59,10 +59,12 @@ def recursive_path(n, i, j, count)
   if i == n-1 && j == n-1
     return count += 1
   end
-  i += 1
-  if j < i
-    j += 1
+  for i in n-1
+    for j in n-1
+      if j < i
+        j += 1
+      end
+    end
   end
   return recursive_path(n, i, j, count)
-  return count
 end
