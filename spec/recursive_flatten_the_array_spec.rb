e69baeea99f5c228dc7c944d131689c57e0a03f7
Kala Wetzler <kala.wetzler@gmail.com>
2017-12-18 15:50:05 -0800

    Add new tests n pass em all

--- spec/recursive_flatten_the_array_spec.rb
+++ spec/recursive_flatten_the_array_spec.rb
@@ -7,4 +7,20 @@ describe '#recursive_flatten_the_array' do
     expect(recursive_flatten_the_array([1,[2],[3, [[4]]]], [])).to eq [1, 2, 3, 4]
   end
 
+  it 'turns a 2d array into a regular array' do
+    expect(recursive_flatten_the_array([[3], 6, [9], [12]], [])).to eq [3, 6, 9, 12]
+  end
+
+  it 'turns a 2d array into a regular array' do
+    expect(recursive_flatten_the_array([[1, 2, [3, 4]]], [])).to eq [1, 2, 3, 4]
+  end
+
+  it 'turns a 2d array into a regular array' do
+    expect(recursive_flatten_the_array([3, [6, [9, [12]]]], [])).to eq [3, 6, 9, 12]
+  end
+
+  it 'turns a 2d array into a regular array' do
+    expect(recursive_flatten_the_array([[1], [2], [3], [4]], [])).to eq [1, 2, 3, 4]
+  end
+
 end
