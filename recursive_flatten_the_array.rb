8097fe2a0e7ed0bc50d3fcaa7b1bc3dc40c078b9
Kala Wetzler <kala.wetzler@gmail.com>
2017-12-18 15:05:42 -0800

    First iteration

--- recursive_flatten_the_array.rb
+++ recursive_flatten_the_array.rb
@@ -3,5 +3,13 @@
 # => [1,2,3,4]
 
 def recursive_flatten_the_array(nested)
-  
+  flattened = []
+  i = 0
+  while i < nested.length - 1
+    if nested[i] != '[' && nested[i] != ']'
+      flattened << nested[i]
+    end
+    i += 1
+  end
+  return flattened
 end
