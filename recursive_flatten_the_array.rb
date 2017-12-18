bc4c86f1e0b8c1ba09fc9512e022b8670839f205
Kala Wetzler <kala.wetzler@gmail.com>
2017-12-18 15:36:11 -0800

    New attempt... not working

--- recursive_flatten_the_array.rb
+++ recursive_flatten_the_array.rb
@@ -2,14 +2,17 @@
 # flatten([1,[2],[3, [[4]]]]);
 # => [1,2,3,4]
 
-def recursive_flatten_the_array(nested)
-  flattened = []
+def recursive_flatten_the_array(nested, flattened)
   i = 0
-  while i < nested.length - 1
-    if nested[i] != '[' && nested[i] != ']'
+  while i < nested.length
+    if nested[i+1] == nil && nested[i].class != Array
+      return flattened
+    end
+    if nested[i].class != Array
       flattened << nested[i]
+    else
+      recursive_flatten_the_array(nested, flattened)
     end
     i += 1
   end
-  return flattened
 end
--- spec/recursive_flatten_the_array_spec.rb
+++ spec/recursive_flatten_the_array_spec.rb
@@ -4,7 +4,7 @@ require_relative '../recursive_flatten_the_array'
 describe '#recursive_flatten_the_array' do
 
   it 'turns a 2d array into a regular array' do
-    expect(recursive_flatten_the_array([1,[2],[3, [[4]]]])).to eq [1, 2, 3, 4]
+    expect(recursive_flatten_the_array([1,[2],[3, [[4]]]], [])).to eq [1, 2, 3, 4]
   end
 
 end
