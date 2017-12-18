59134ae7ad36da65f62e8ece2995e0732a2c6f3e
Kala Wetzler <kala.wetzler@gmail.com>
2017-12-18 15:44:55 -0800

    Pass test. Need more tests...

--- recursive_flatten_the_array.rb
+++ recursive_flatten_the_array.rb
@@ -14,17 +14,3 @@ def recursive_flatten_the_array(nested, flattened)
   end
   flattened
 end
-
-__END__
-__CALL STACK__
-
-i = 0
-while i < nested.length
-  if nested[i].class != Array
-    flattened << nested[i]
-  else
-    recursive_flatten_the_array(flattened, [])
-  end
-  i += 1
-end
-flattened
