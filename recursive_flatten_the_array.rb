baed4e8c4135f12eada62d83854d6520d36b07d3
Kala Wetzler <kala.wetzler@gmail.com>
2017-12-18 15:43:39 -0800

    woo.. now the recursive params should be fixed

--- recursive_flatten_the_array.rb
+++ recursive_flatten_the_array.rb
@@ -8,9 +8,23 @@ def recursive_flatten_the_array(nested, flattened)
     if nested[i].class != Array
       flattened << nested[i]
     else
-      recursive_flatten_the_array(flattened, [])
+      recursive_flatten_the_array(nested[i], flattened)
     end
     i += 1
   end
   flattened
 end
+
+__END__
+__CALL STACK__
+
+i = 0
+while i < nested.length
+  if nested[i].class != Array
+    flattened << nested[i]
+  else
+    recursive_flatten_the_array(flattened, [])
+  end
+  i += 1
+end
+flattened
