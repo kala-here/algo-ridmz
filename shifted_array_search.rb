5dbc7e43d7070dc7b6c3177de5c12b808f461b3a
Kala Wetzler <kala.wetzler@gmail.com>
2017-12-18 18:31:39 -0800

    Consider pivot point

--- shifted_array_search.rb
+++ shifted_array_search.rb
@@ -1,7 +1,22 @@
 # given a shifted array of an unknown offset (that is otherwise sorted from smallest to largest), find an efficient way to return the index of num
 
 def shifted_array_search(arr, num)
-  # this will be a binary search
+  # this will be a binary search to find the pivot point
+  n = arr.length - 1
+  mid = n/2
+  if num == arr[mid]
+    return arr[mid]
+  end
+  if num == arr[mid-1]
+    return arr[mid-1]
+  end
+  if arr[mid] > arr[mid-1]
+    new_arr = arr[mid..n]
+    shifted_array_search(new_arr, num)
+  end
+end
+
+
 
 end
 
