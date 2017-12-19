fc27f302c3b9bd4db684e93feb0267836e808993
Kala Wetzler <kala.wetzler@gmail.com>
2017-12-18 18:50:12 -0800

    Need to find indexwithin previous array...

--- shifted_array_search.rb
+++ shifted_array_search.rb
@@ -2,41 +2,21 @@
 
 def shifted_array_search(arr, num)
   # this will be a binary search to find the pivot point
-  n = arr.length - 1
-  mid = n/2
+  n = arr.length - 1 # 4
+  mid = n/2 # 2
   if num == arr[mid] # false
-    return arr[mid]
+    p mid
+    return mid
   end
   if num == arr[mid-1] # false
-    return arr[mid-1]
+    return mid-1
   end
-  if arr[mid] > arr[mid-1] # false
-    new_arr = arr[mid..n]
+  if arr[mid] > arr[mid-1] # true
+    new_arr = arr[mid..n] # [14,10,11]
     shifted_array_search(new_arr, num)
   end
-  if arr[mid] < arr[mid-1] # true
-    new_arr = arr[0..mid] # [4, 6, 1]
+  if arr[mid] < arr[mid-1] #
+    new_arr = arr[0..mid] #
     shifted_array_search(new_arr, num)
   end
-end
-
-
-
-end
-
-__END__
-[4, 5, 6, 1, 2], 2
-[4, 6, 1, 2, 3], 4
-n = arr.length - 1 # 4
-mid_i = n/2 # 2
-if num == arr[mid_i] # false
-  return mid_i
-end
-if num > arr[mid_i]
-  upper_half = arr[mid_i..n]
-  shifted_array_search(upper_half, num)
-end
-if num < arr[mid_i]
-  lower_half = arr[0..mid_i]
-  shifted_array_search(lower_half, num)
 end
