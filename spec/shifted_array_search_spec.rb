9a51a2dd00387b24efcdcd7669f495118295794e
Kala Wetzler <kala.wetzler@gmail.com>
2017-12-18 17:13:19 -0800

    Add tests

---
+++ spec/shifted_array_search_spec.rb
@@ -0,0 +1,31 @@
+require 'rspec'
+require_relative '../shifted_array_search'
+
+describe '#shifted_array_search' do
+
+  it 'returns the index of 5' do
+    x = [3, 4, 5, 6, 1, 2]
+    expect(shifted_array_search(x, 5)).to eq 2
+  end
+
+  it 'returns the index of 2' do
+    x = [4, 5, 6, 1, 2]
+    expect(shifted_array_search(x, 2)).to eq 4
+  end
+
+  it 'returns the index of 1' do
+    x = [1, 2, 3, 4]
+    expect(shifted_array_search(x, 1)).to eq 0
+  end
+
+  it 'returns the index of 3' do
+    x = [7, 1, 2, 3, 4, 5, 6]
+    expect(shifted_array_search(x, 3)).to eq 3
+  end
+
+  it 'returns the index of 5' do
+    x = [12, 13, 14, 10, 11]
+    expect(shifted_array_search(x, 10)).to eq 3
+  end
+
+end
