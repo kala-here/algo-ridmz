f3c664db3ea3a3018f4dc2ddf4bdf0619c2dbda3
Kala Wetzler <kala.wetzler@gmail.com>
2017-12-19 05:26:03 -0800

    Add functionality for shifted array method - now implement binary search

--- shifted_array_search.rb
+++ shifted_array_search.rb
@@ -1,9 +1,42 @@
 # given a shifted array of an unknown offset (that is otherwise sorted from smallest to largest), find an efficient way to return the index of num
 
 def shifted_array_search(arr, num)
+  if arr.length == 1
+    return arr[0]
+  end
+  pivot_point = find_pivot(arr)
+  if num == pivot_point
+    return pivot_point
+  end
+  if num < arr[0]
+    return binary_search(arr, pivot_point, arr.length - 1, num)
+  end
+  if num > arr[0]
+    return binary_search(arr, 0, pivot_point - 1, num)
+  end
+end
 
 end
 
+def binary_search(arr, start, finish, num)
+  n = arr.length - 1
+  start = 0
+  mid = pivot
+  finish = arr[n]
+  if num == pivot
+    return pivot
+  end
+  if num > pivot #search left
+    upper_half = arr[start]
+    binary_search(upper_half, num)
+  end
+  if num < arr[mid_i] #search right
+    lower_half = arr[0..mid_i]
+    shifted_array_search(lower_half, num)
+  end
+end
+
+# input = array, output = index of pivot point
 def find_pivot(arr)
   n = arr.length - 1
   start = 0
@@ -25,17 +58,4 @@ end
 __END__
 [4, 5, 6, 1, 2], 2
 [12, 13, 14, 10, 11], 10
-
-n = arr.length - 1 # 4
-mid_i = n/2 # 2
-if num == arr[mid_i] # false
-  return mid_i
-end
-if num > arr[mid_i]
-  upper_half = arr[mid_i..n]
-  shifted_array_search(upper_half, num)
-end
-if num < arr[mid_i]
-  lower_half = arr[0..mid_i]
-  shifted_array_search(lower_half, num)
-end
+[1, 2, 3, 4], 1
