1d70538554e15fdda5d1d45afb784014fecc8197
Kala Wetzler <kala.wetzler@gmail.com>
2017-12-18 17:25:03 -0800

    Pass 2 tests

--- shifted_array_search.rb
+++ shifted_array_search.rb
@@ -1,4 +1,4 @@
-# given a shifted array of an unknown offset (that is otherwise sorted), find an efficient way to return the index of num
+# given a shifted array of an unknown offset (that is otherwise sorted from smallest to largest), find an efficient way to return the index of num
 
 def shifted_array_search(arr, num)
   # this will be a binary search
@@ -6,5 +6,13 @@ def shifted_array_search(arr, num)
   mid_i = n/2
   if num == arr[mid_i]
     return mid_i
+  end
+  if num > arr[mid_i]
+    upper_half = arr[mid_i...n-1]
+    shifted_array_search(upper_half, num)
+  end
+  if num < arr[mid_i]
+    lower_half = arr[0...mid_i]
+    shifted_array_search(lower_half, num)
   end
 end
