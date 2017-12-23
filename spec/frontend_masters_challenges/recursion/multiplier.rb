8e0ac40bd62e4e1a00d170d9e67b9fdef95c33b8
Kala Wetzler <kala.wetzler@gmail.com>
2017-12-23 10:58:21 -0800

    Get set up with question and a couple tests

---
+++ spec/frontend_masters_challenges/recursion/multiplier.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+require_relative '../../../frontend_masters_challenges/recursion/multiplier'
+
+define '#recursive_multiplier' do
+
+  it 'returns an array of array items multiplied by num'
+    expect(recursive_multiplier([1, 2, 3], 3)).to eq [3, 6, 9]
+  end
+
+  it 'returns an array of array items multiplied by num'
+    expect(recursive_multiplier([4, 1, 2], 2)).to eq [8, 2, 4]
+  end
+end
