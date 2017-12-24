2ec7673c07f4464cdf0d122da381e197ae189866
Kala Wetzler <kala.wetzler@gmail.com>
2017-12-23 20:12:09 -0800

    Attempt to pass tests

--- frontend_masters_challenges/min_stack.rb
+++ frontend_masters_challenges/min_stack.rb
@@ -2,6 +2,18 @@
 
 require_relative '../stack'
 
-def min_stack(stack)
-  
+def min_stack(arr)
+  stack = Stack.new(arr)
+  min_track = Stack.new([]])
+  # Initialize min_track with first apparent lowest
+  min_track.push(stack.pop)
+  while stack.size != 0
+    if min_track.peek <= stack.peek
+      min_track.push(stack.pop)
+    else
+      min_track.push(min_track.peek)
+      stack.pop
+    end
+  end
+  return min_track.peek
 end
