1a7fb8183b85b75906e4adbcb86a38b044acf8b1
Kala Wetzler <kala.wetzler@gmail.com>
2017-12-25 08:38:51 -0800

    First off: Outsource

--- interview_cake_challenges/million_gazillion.rb
+++ interview_cake_challenges/million_gazillion.rb
@@ -5,3 +5,9 @@
 # Thing is, the crawler is running on my old desktop computer in my parents' basement (where I totally don't live anymore), and it keeps running out of memory because visited is getting so huge.
 
 # How can I trim down the amount of space taken up by visited?
+
+# ______________________________________________________
+
+# PSEUDOCODE
+# First off, I would outsource the storage so that it lives in the cloud where I can monitor and scale the space to what I need.
+# Second, so that I am not paying more than I need to, I would 
