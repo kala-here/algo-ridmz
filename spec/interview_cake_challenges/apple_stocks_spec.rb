af1b361c80de2213879895f176fb4860d2a2e5e0
Kala Wetzler <kala.wetzler@gmail.com>
2017-12-27 12:56:59 -0800

    Add question

--- spec/interview_cake_challenges/apple_stocks_spec.rb
+++ spec/interview_cake_challenges/apple_stocks_spec.rb
@@ -4,6 +4,6 @@ require_relative '../../interview_cake_challenges/apple_stocks'
 describe '#get_max_profit' do
   it 'returns the max profit' do
     stock_prices_yesterday = [10, 7, 5, 8, 11, 9]
-    expect(get_max_profit(stock_prices_yesterday)).to eq
+    expect(get_max_profit(stock_prices_yesterday)).to eq 6
   end
 end
