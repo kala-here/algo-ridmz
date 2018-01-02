5019a50aacbfef2463083ad0e496acfbbd19eb09
Kala Wetzler <kala.wetzler@gmail.com>
2018-01-02 10:01:28 -0800

    Add brute force pseudocode

--- interview_cake_challenges/merging_ranges.rb
+++ interview_cake_challenges/merging_ranges.rb
@@ -21,6 +21,14 @@
 
 # Write a solution that's efficient even when we can't put a nice upper bound on the numbers representing our time ranges. Here we've simplified our times down to the number of 30-minute slots past 9:00 am. But we want the method to work even for very large numbers, like Unix timestamps. In any case, the spirit of the challenge is to merge meetings where start_time and end_time don't have an upper bound.
 
+# PSEUDOCODE:
+# Brute force: for each nested arr, push items between ranges into new arr
+  # if items already exist in the arr, skip it
+# sort the arr, then find gaps in the arr and make new ranges based on that
+# time complexity for building the arr = O(n); for sorting it, it would be O(n*log(n)); for searching for gaps, it would be O(n) because we are going to be looking at each item in the array and it's neighbor. SO THE TIME COMPLEXITY WOULD BE O(N*LOG(N))
+# space complexity would be O(n) to build the new arr and again O(n) to build the new ranges. SO SPACE COMPLEXITY WOULD BE O(N)
+
+
 def merge_ranges(timeslots)
-  # my code goes here
+
 end
