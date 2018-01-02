83f18afb68ae51ed27bc5b63e5504ffd69a5327a
Kala Wetzler <kala.wetzler@gmail.com>
2018-01-01 19:18:56 -0800

    Tests with negs in the examples are not working

--- interview_cake_challenges/highest_product_of_3.rb
+++ interview_cake_challenges/highest_product_of_3.rb
@@ -9,9 +9,9 @@ def highest_product_of_3(arr)
     return "There must be at least 3 items in the array to make a product out of."
   end
   sorted = arr.sort!
-  if sorted[0] >= 0 || sorted[n - 1] < 0
+  if sorted[0] >= 0 || sorted[-1] < 0
     # we will grab the last 3 items in the arr and mult them together
-    return sorted[n-1] * sorted[n-2] * sorted[n-3]
+    return sorted[-1] * sorted[-2] * sorted[-3]
   end
   # neg * neg = pos
   return "help"
