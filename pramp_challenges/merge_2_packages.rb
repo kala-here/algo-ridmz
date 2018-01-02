592c7993f30217b0efca7517d1e16fa49d426d19
Kala Wetzler <kala.wetzler@gmail.com>
2018-01-01 21:26:32 -0800

    Get set up

---
+++ pramp_challenges/merge_2_packages.rb
@@ -0,0 +1,8 @@
+# Merging 2 Packages
+# Given a package with a weight limit and an array of item weights, implement a function getIndicesOfItemWeights that finds two items whose sum of weights equals the weight limit. Your function should return a pair [i, j] of the indices of the item weights, ordered such that i < j. If such a pair doesn’t exist, return an empty array.
+
+# Analyze the time and space complexities of your solution.
+
+def get_indices_of_item_weights(arr, limit)
+  # your code goes here
+end
