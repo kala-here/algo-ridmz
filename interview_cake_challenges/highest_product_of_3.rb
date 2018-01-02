d3e1c6b97d17bba5ae0122b5f3e3fda2e8b9b342
Kala Wetzler <kala.wetzler@gmail.com>
2018-01-01 19:00:24 -0800

    Bring the return statement back

--- interview_cake_challenges/highest_product_of_3.rb
+++ interview_cake_challenges/highest_product_of_3.rb
@@ -3,14 +3,13 @@
 # The input array_of_ints will always have at least three integers.
 
 def highest_product_of_3(arr)
+  p arr
   n = arr.length
   if n < 3
-    raise Exception, "There must be at least 3 items in the array to make a product out of."
+    return "There must be at least 3 items in the array to make a product out of."
   end
   sorted = arr.sort!
   if sorted[0] >= 0 || sorted[n - 1] < 0
-    p sorted
-    p sorted[n-1]
     # we will grab the last 3 items in the arr and mult them together
     return sorted[n-1] * sorted[n-2] * sorted[n-3]
   end
