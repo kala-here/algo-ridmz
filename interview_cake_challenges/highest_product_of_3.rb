d2dfe5fe83f60405cb6df2cd171be14977422a51
Kala Wetzler <kala.wetzler@gmail.com>
2018-01-01 18:19:39 -0800

    Account for an arr of all negative nums

--- interview_cake_challenges/highest_product_of_3.rb
+++ interview_cake_challenges/highest_product_of_3.rb
@@ -8,9 +8,11 @@ def highest_product_of_3(arr)
     return "There must be at least 3 items in the array to make a product out of."
   end
   sorted = arr.sort!
-  if sorted[0] >= 0
+  if sorted[0] >= 0 || sorted[n - 1] < 0
     # we will grab the last 3 items in the arr and mult them together
     return sorted[n-1] * sorted[n-2] * sorted[n-3]
   end
-  return "there are negs"
+  # neg * neg = pos
+  # [−10,−10,1,2,3]   [-10, -9, -8, -7]
+
 end
