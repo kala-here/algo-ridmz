9437d2f85e98f470c4b6f3c3bd3938287f96a34a
Kala Wetzler <kala.wetzler@gmail.com>
2018-01-13 13:24:34 -0800

    Add pseudocode

---
+++ other_challenges/shuffle_deck.rb
@@ -0,0 +1,7 @@
+# Shuffle a deck of cards
+
+def shuffle_deck(deck)
+  # cut deck into 2 separate arrays
+  # pop off random num of cards between (1-7) and push into shuffed_deck
+  # return shuffled_deck
+end
